b79d32db4cbd7c221dbf5b81ab3c7763876f8b88
CC11001100 <CC11001100@qq.com>
2022-10-22 15:47:37 +0800

    init

---
+++ duration.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cast"
+)
+
+// 转为可读形式
+
+type Foo string
+
+func main() {
+	var foo Foo = "asd"
+	toString, err := cast.ToStringE(foo)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(toString)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/CC11001100/go-cast-anything
+
+go 1.19
+
+require github.com/spf13/cast v1.5.0 // indirect
